bc24833955251e01103bdd4d0b76b0cd0c2b7440
Kalamaushika Vikna <kalamaushika.vikna@gmail.com>
2014-06-07 10:52:58 -0500

    added home controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Caux::Application.config.secret_key_base = 'ce9d3571cf791710c8d5dc252e5f650c468b5ef0d05bcc3beb7ecd527cc57d6c5f68e1bc254afaba815834e7b251f550fd4f4271043a84497148cd1a71e9e055'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Caux::Application.config.session_store :cookie_store, key: '_caux_session'
